f13969523c56751109680fcf2a6a6d75c37aef9d
Erik Karsten <erik@opcode.xyz>
2023-08-06 10:07:26 +0300

    Implement modmenu compat

---
+++ src/client/java/com/sheb/filtertools/compat/ModMenuImpl.java
@@ -0,0 +1,17 @@
+package com.sheb.filtertools.compat;
+
+import com.sheb.filtertools.FilterToolsConfigGui;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+public class ModMenuImpl implements ModMenuApi
+{
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory()
+    {
+        return (screen) -> {
+            FilterToolsConfigGui gui = new FilterToolsConfigGui();
+            gui.setParent(screen);
+            return gui;
+        };
+    }
+}
